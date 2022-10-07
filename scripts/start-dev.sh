c4327b2667ded1be75099043a75a261d00a126f4
Már Örlygsson <mar.nospam@anomy.net>
2022-10-07 12:04:59 +0000

    docs: Fix typo in script comment

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# Extracting this into a standalone *.sh file (and NOT using yarn to invode
+# Extracting this into a standalone *.sh file (and NOT using yarn to invoke
 # the cssserve binary) is required to make it possible to start the
 # style server from an external repo/project — such as the hanna monorepo
 node node_modules/.bin/cssserve --config cssserve-dev.json
